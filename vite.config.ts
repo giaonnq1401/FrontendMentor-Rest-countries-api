15dad2db33065ab745c5bb60f3d164216b286239
giaonnq1401 <giaonguyennq142001@gmail.com>
2023-12-28 22:32:42 +0700

    add package, tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import react from '@vitejs/plugin-react';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const dir = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(dir, 'src'),
+    },
+  },
 });
